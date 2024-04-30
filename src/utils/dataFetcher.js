ae92b8734ea8f6411d157e98110b20e45dce4c04
ElvisMw <elvismwanthi@gmail.com>
2024-04-30 21:00:38 +0300

    readme

--- src/utils/dataFetcher.js
+++
@@ -1,9 +0,0 @@
-const fetch = require("node-fetch");
-
-const fetchData = async (url) => {
-  let val = await fetch(url)
-    .then((res) => res.json())
-    .catch((err) => err);
-  return val;
-};
-module.exports = fetchData;
